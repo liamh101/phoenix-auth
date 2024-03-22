474ffdc5781f0e7d622969b7fce163222a783da3
Liam Hackett <liamjhackett@gmail.com>
2024-03-22 23:07:08 +0000

    Change copyToClipboard to always get the current token

--- src/components/accounts/AccountItem.vue
+++ src/components/accounts/AccountItem.vue
@@ -2,16 +2,18 @@
 
 import DeleteAccount from "./DeleteAccount.vue";
 import OneTimePassword from "./OneTimePassword.vue";
-import {copyTextToClipboard} from "../../composables/Generics.ts";
+import {copyOtpToClipboard, copyTextToClipboard} from "../../composables/Generics.ts";
 import CountdownTimer from "./CountdownTimer.vue";
 import {computed} from "vue";
 
 const props = defineProps({
   accountId: {
     type: Number,
+    required: true,
   },
   accountName: {
     type: String,
+    required: true,
   },
   manage: {
     type: Boolean,
@@ -21,18 +23,12 @@ const props = defineProps({
 
 const emit = defineEmits(['accountRemoved']);
 
-let currentToken: string = '';
-
 function accountRemoved() {
   emit('accountRemoved')
 }
 
 async function copyToClipboard() {
-  await copyTextToClipboard(currentToken);
-}
-
-function setCurrentToken(token: string) {
-  currentToken = token;
+  await copyOtpToClipboard(props.accountId);
 }
 
 </script>
@@ -47,7 +43,7 @@ function setCurrentToken(token: string) {
         <h2>{{props.accountName}}</h2>
       </div>
       <div class="col-5">
-        <one-time-password v-if="!props.manage" :account-id="props.accountId" @otp="setCurrentToken"/>
+        <one-time-password v-if="!props.manage" :account-id="props.accountId"/>
 
         <delete-account v-if="props.manage" :account-id="props.accountId" @success="accountRemoved"/>
       </div>
--- src/composables/Generics.ts
+++ src/composables/Generics.ts
@@ -1,7 +1,8 @@
+import {generateToken} from "./Commands.ts";
 
-export async function copyTextToClipboard(value: string): Promise<void> {
+export async function copyOtpToClipboard(accountId: number): Promise<void> {
     try {
-        await navigator.clipboard.writeText(value);
+        await navigator.clipboard.writeText((await generateToken(accountId)).token);
     } catch (e) {
         console.error(e)
     }
