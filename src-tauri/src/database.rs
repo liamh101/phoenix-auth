69b79fd3f70961e84428f9e1bac30b99efc14564
Liam Hackett <liamjhackett@gmail.com>
2024-10-02 22:27:37 +0100

    Add additional tests

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -436,8 +436,8 @@ fn update_database(db: &mut Connection, existing_version: u32) -> Result<(), rus
 #[cfg(test)]
 mod tests {
     use libotp::HOTPAlgorithm;
-    use magic_crypt::generic_array::typenum::assert_type;
-    use crate::database::{AccountAlgorithm, create_new_account, initialize_test_database, update_existing_account};
+    use rusqlite::Connection;
+    use crate::database::{AccountAlgorithm, create_new_account, get_all_accounts, initialize_test_database, SQLITE_TEST_NAME, update_existing_account};
     use crate::database::AccountAlgorithm::{SHA1, SHA512};
 
     #[test]
@@ -489,7 +489,7 @@ mod tests {
     #[test]
     fn create_new_account_full() {
         let db = initialize_test_database().unwrap();
-        let name = "Full Test";
+        let name = "New Full Test";
         let secret = "HelloWorld";
         let digits = 8;
         let step = 30;
@@ -590,5 +590,66 @@ mod tests {
         assert_eq!(original_account.totp_step, updated_account.totp_step);
 
         assert_eq!(true, updated_account.algorithm.is_none());
+    }
+
+    #[test]
+    fn get_all_accounts_order() {
+        let db = initialize_test_database().unwrap();
+        reset_db(&db);
+
+        let expected_second = create_new_account("AB Record", "1234", &8, &30, "", &db).unwrap();
+        let expected_third = create_new_account("AC Record", "2134", &4, &15, "", &db).unwrap();
+        let expected_first = create_new_account("AA Record", "9284", &12, &60, "", &db).unwrap();
+
+        let result = get_all_accounts(&db, "");
+
+        assert_eq!(true, result.is_ok());
+
+        let accounts = result.unwrap();
+
+        assert_eq!(3, accounts.len());
+
+        assert_eq!(expected_first.id, accounts[0].id);
+        assert_eq!(expected_first.name, accounts[0].name);
+        assert_eq!("", accounts[0].secret);
+
+        assert_eq!(expected_second.id, accounts[1].id);
+        assert_eq!(expected_second.name, accounts[1].name);
+        assert_eq!("", accounts[1].secret);
+
+        assert_eq!(expected_third.id, accounts[2].id);
+        assert_eq!(expected_third.name, accounts[2].name);
+        assert_eq!("", accounts[2].secret);
+    }
+
+    // fn get_account_details_by_id() {
+    //
+    // }
+    //
+    // fn get_account_details_by_id_with_external_details() {
+    //
+    // }
+    //
+    // fn set_external_id_to_account() {
+    //
+    // }
+    //
+    // fn set_sync_accounts() {
+    //
+    // }
+    //
+    // fn update_sync_accounts() {
+    //
+    // }
+    //
+    // fn delete_sync_accounts() {
+    //
+    // }
+
+    fn reset_db(db: &Connection) -> Result<(), rusqlite::Error> {
+        let mut statement = db.prepare("DELETE FROM accounts")?;
+        statement.execute([])?;
+
+        Ok(())
     }
 }
