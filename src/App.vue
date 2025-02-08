2cc9a271170b9277cc63d340b51ea02c2215d0c1
Liam Hackett <liamjhackett@gmail.com>
2025-02-08 22:19:24 +0000

    Merge theme component into the App component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import AccountPage from "./components/accounts/AccountPage.vue";
-import {ref} from "vue";
+import {ref, onMounted} from "vue";
 import SettingsPage from "./components/settings/SettingsPage.vue";
-import Theme from "./components/Theme.vue";
-import {THEME_MODES} from "./composables/Commands.ts";
+import {getSettings, THEME_MODES} from "./composables/Commands.ts";
+import {getOsTheme} from "./composables/OperatingSystem.ts";
 
 let displayAccountPage = ref(true);
 let displaySettingsPage = ref(false);
@@ -19,25 +19,55 @@ function settingsPageSelected() {
   displaySettingsPage.value = true;
 }
 
-function themeChanged(theme: THEME_MODES) {
-  override_theme.value = theme;
+async function setupTheme() {
+  const settings = await getSettings();
+
+  if (settings.settings.theme === THEME_MODES.DEFAULT) {
+    const systemTheme = await getOsTheme();
+
+    setThemeAttributes(systemTheme)
+    return;
+  }
+
+  const userTheme = getThemeFromMode(settings.settings.theme);
+
+  setThemeAttributes(userTheme)
+  return;
 }
 
+function getThemeFromMode(settingTheme: THEME_MODES) {
+  if (settingTheme === THEME_MODES.LIGHT) {
+    return 'light'
+  }
+
+  return 'dark';
+}
+
+function setThemeAttributes(theme: string) {
+  document.documentElement.setAttribute("data-theme", theme);
+  document.documentElement.setAttribute("data-bs-theme", theme);
+}
+
+function init() {
+  setupTheme()
+}
+
+onMounted(() => init())
 </script>
 
 <template>
   <div>
-    <theme :override-theme="override_theme"/>
+    <theme :override-theme="override_theme" />
 
     <account-page
-        v-if="displayAccountPage"
-        @show-settings="settingsPageSelected"
+      v-if="displayAccountPage"
+      @show-settings="settingsPageSelected"
     />
 
     <settings-page
-        v-if="displaySettingsPage"
-        @show-tokens="accountPageSelected"
-        @change-theme="themeChanged"
+      v-if="displaySettingsPage"
+      @show-tokens="accountPageSelected"
+      @change-theme="setupTheme"
     />
   </div>
 </template>
